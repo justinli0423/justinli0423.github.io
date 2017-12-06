bde683b4b58d731d49874c0e3f68a719762b67bb
jli0423 <justin.li0423@gmail.com>
2017-12-06 01:29:42 -0500

    Timeline Styling complete

---
+++ js/page-load.js
@@ -0,0 +1,4 @@
+// load page on top
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
---
+++ js/timeline.js
@@ -0,0 +1,40 @@
+var items = document.getElementsByClassName("list-element")
+
+function isElementInViewport(el) {
+    // returns the smallest rectangle that bounds the element
+    var rect = el.getBoundingClientRect();
+    return (
+        // return true if all sides are within viewport, otherwise return false
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function isElementOutOfPort(el) {
+    // returns the smallest rectangle that bounds the element
+    var rect = el.getBoundingClientRect();
+    return (
+        // return true if bottom is within bounds
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
+function callbackFunc() {
+    for(var i = 0; i < items.length; i++){
+        if(isElementInViewport(items[i])) {
+            items[i].classList.add("in-view");
+        }
+        else if(!isElementOutOfPort(items[i])){
+            items[i].classList.remove("in-view");
+        }
+
+    }
+}
+
+
+// run function on scroll and page load
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+window.addEventListener("resize", callbackFunc);
