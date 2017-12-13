90f4d1df2bc60b58c4cf31ccc009cf70d5f5b412
jli0423 <justin.li0423@gmail.com>
2017-12-12 19:26:22 -0500

    Modified navi btn appear height requirement

--- js/navi.js
+++ js/navi.js
@@ -1,6 +1,7 @@
 $(document).scroll(function() {
     var y = $(this).scrollTop();
-    if (y > 70) {
+    var x = $(this).width();
+    if ((y > 70 && x > 900) || (y > 110 && x > 612)) {
       $('.navigation-toggle').fadeIn();
     } else {
       $('.navigation-toggle').fadeOut();
