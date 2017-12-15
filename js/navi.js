fe7678586ef7b4672e73fa42a37feb97133b239b
jli0423 <justin.li0423@gmail.com>
2017-12-15 15:30:29 -0500

    reversed navi top order

--- js/navi.js
+++ js/navi.js
@@ -1,14 +1,14 @@
-$(document).scroll(function () {
-  var y = $(this).scrollTop();
-  var x = $(this).width();
-  if (y > 70) {
-    $('.navigation-toggle').fadeIn();
-  } else {
-    $('.navigation-toggle').fadeOut();
-  }
+// $(document).scroll(function () {
+//   var y = $(this).scrollTop();
+//   var x = $(this).width();
+//   if (y > 70) {
+//     $('.navigation-toggle').fadeIn();
+//   } else {
+//     $('.navigation-toggle').fadeOut();
+//   }
 
-});
+// });
 
-$(document).ready(function () {
-  $('.navigation-toggle').fadeOut();
-});
+// $(document).ready(function () {
+//   $('.navigation-toggle').fadeOut();
+// });
