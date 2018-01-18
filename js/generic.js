fce2c097a6e7b9ad6838e28b7e167d10c49f164e
Justin Li <justin.li0423@gmail.com>
2018-01-18 12:35:11 -0500

    fixed indexes

--- js/generic.js
+++ js/generic.js
@@ -1,5 +1,19 @@
 // Smooth Scrolling
-$(document).on('click', 'a[href^="#"]', function (event) {
+$(document).on('click', 'a[href^="#timeline"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+$(document).on('click', 'a[href^="#top"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+$(document).on('click', 'a[href^="#projects"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
