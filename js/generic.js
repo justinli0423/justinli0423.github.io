f5dad7d16ff6bed77940ea97579f16c4a019c2ec
Justin Li <justin.li0423@gmail.com>
2018-02-09 20:49:10 -0500

    Added intro animation

--- js/generic.js
+++ js/generic.js
@@ -1,5 +1,23 @@
+// Header timeout
+setTimeout(() => {
+    $('.loading__animation').addClass('hidden');
+}, 2000);
+setTimeout(() => {
+    $('.overflow').removeClass('overflow');
+    $('.loading').addClass('hidden');
+}, 3000);
+
 // Smooth Scrolling - per ID
-$(document).on('click', 'a[href^="#timeline"]', function (event) {
+$(document).on('click', 'a[href^="#timeline"]', (event) => {
+    event.preventDefault(); 
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+
+
+$(document).on('click', 'a[href^="#top"]', (event) =>  {
     event.preventDefault();
 
     $('html, body').animate({
@@ -8,16 +26,7 @@ $(document).on('click', 'a[href^="#timeline"]', function (event) {
 });
 
 
-$(document).on('click', 'a[href^="#top"]', function (event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 800);
-});
-
-
-$(document).on('click', 'a[href^="#projects"]', function (event) {
+$(document).on('click', 'a[href^="#projects"]', (event) =>  {
     event.preventDefault();
 
     $('html, body').animate({
@@ -26,11 +35,11 @@ $(document).on('click', 'a[href^="#projects"]', function (event) {
 });
 
 // particle js
-particlesJS.load('particles-js', 'json/particles.json', function () {
+particlesJS.load('particles-js', 'json/particles.json', () => {
     console.log('callback - particles.js config loaded');
 });
 
 // load page on top
-window.onbeforeunload = function () {
+window.onbeforeunload = () => {
     window.scrollTo(0, 0);
 }
