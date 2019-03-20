1f916805077325d805ad5c34202d5d2c6e0a17dc
Justin Li <justin.li0423@gmail.com>
2019-03-20 00:32:23 -0400

    Fixed problems

--- js/generic.js
+++ js/generic.js
@@ -1,5 +1,5 @@
 // Smooth Scrolling - per ID
-$(document).on('click', 'a[href^="#timeline"]', function (event) {
+$(document).on('click', 'a[href^="#timeline"]', (event) => {
     event.preventDefault();
 
     $('html, body').animate({
@@ -8,7 +8,7 @@ $(document).on('click', 'a[href^="#timeline"]', function (event) {
 });
 
 
-$(document).on('click', 'a[href^="#top"]', function (event) {
+$(document).on('click', 'a[href^="#top"]', (event) => {
     event.preventDefault();
 
     $('html, body').animate({
@@ -17,12 +17,22 @@ $(document).on('click', 'a[href^="#top"]', function (event) {
 });
 
 
-$(document).on('click', 'a[href^="#projects"]', function (event) {
+$(document).on('click', 'a[href^="#projects"]', (event) => {
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 800);
+});
+
+// Remove scroll when projects open
+$(document).on('click', '.resource__icon-stack', (event) => {
+    $('html, body').addClass('overflow');
+});
+
+// add scroll when projects close
+$(document).on('click', '.popup__close', (event) => {
+    $('html, body').removeClass('overflow');
 });
 
 // load page on top
